02534fff501d985627cb26f7ece9798e70d4af25
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-02-05 21:00:03 +0530

    unused CSS removed

--- src/components/showPolls.svelte
+++ src/components/showPolls.svelte
@@ -36,34 +36,4 @@
         grid-template-columns: 1fr 1fr;
         grid-gap: 20px;
     }
-    .card {
-        padding: 20px;
-        background: #fff;
-        box-shadow: 1px 1px 2px rgba(0,0,0, .5);
-    }
-    .card > h4 {
-        margin: 0;
-        padding: 0;
-        margin-bottom: 20px;
-    }
-    .percent-wrapper{
-        margin-bottom: 15px;
-        background: #eeebeb;
-        line-height: 1.5em;
-        position: relative;
-        border: 1px solid transparent;
-    }
-    .percent-wrapper:hover {
-        opacity: .5;
-        border: 1px solid #ddd;
-        cursor: pointer;
-    }
-    .percent-wrapper .percent {
-        position: absolute;
-        top: 0;
-        left: 0;
-        display: inline-block;
-        height: 100%;
-        background: rgba(220, 20, 60, .5);
-    }
 </style>
