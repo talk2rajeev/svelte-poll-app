af8ea367a15abb9ca5933d9f251b32dbcb4d88f2
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-02-17 08:51:15 +0530

    Header icon added

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,7 @@
 
 <main>
     <div class="logo-box">
+        <div><img src="poll.png" style="width: 100px;" alt=""/></div>
         <h3>Poll App</h3>
     </div>
 </main>
@@ -11,8 +12,16 @@
         padding: 20px;
         background-color: #ddd;
         text-align: center;
+        color: #313131;
     }
     .logo-box {
         text-align: center;
+        display: grid;
+        grid-template-columns: 120px auto;
+        justify-content: center;
+    }
+    h3 {
+        margin: 0;
+        font-size: 2.2em;
     }
 </style>
